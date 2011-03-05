5340a163d24f6aa76330adb2c7bb4bde294ccc40
ham <ham@ham-ThinkPad.(none)>
2011-03-05 21:45:26 +0100

    cursor beta

--- cursor.js
+++ cursor.js
@@ -1,6 +1,9 @@
 var cursor = true;
 var cursorInterval = false;
 var ctrl = false;
+var keydown = 0;
+
+var position = 0;
 
 $(document).ready(function() {
 	cursorInterval = setInterval(showCursor, 500);
@@ -25,6 +28,7 @@ $(document).ready(function() {
 	
 	$(window).keydown(function(e) {
 		var code = (e.keyCode ? e.keyCode : e.which);
+		keydown = code;
 		if(code == 17) {
 			ctrl = true;
 		}
@@ -34,21 +38,42 @@ $(document).ready(function() {
 		if (ctrl) return true;
 		
 		var code = (e.keyCode ? e.keyCode : e.which);
-		
-		if(code == 13) {
+
+		if(code == 13) {	//return
 			submit();
-		} else {
+			position = 0;
+		} else if (code == 37) {	//left
+			if (position > 0)
+				position--;
+		} else if (code == 39) {	//right
+			if (position < $("#read").text().length)
+				position++;
+		} else if (code == 46 && keydown == 46) {	//delete
+			if (position < $("#read").text().length) {
+				del(false);
+			}
+		} else if (code == 8) {	//backspace
+			if (position > 0) {
+				del(true);
+				position--;
+			}
+		} else if (code > 31 && code < 127){
 			$("#read").append(String.fromCharCode(code));
+			position++;
 		}
-		
-		var pos = $("#read").text().length;
-		//alert(pos);
-		pos = 8 * (pos);
+
+		pos = 8 * position;
 		$("#read").css("background-position", pos + "px 0");
 		
 		return false;
 	});
 });
+
+function del(back) {
+	var text = $("#read").text();
+	text = text.substring(0, position - (back ? 1 : 0)) + text.substring(position + (back ? 0 : 1));
+	$("#read").text(text);
+}
 
 function showCursor() {
 	if (cursor) {
