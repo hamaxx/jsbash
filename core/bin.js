2592bd60ed7d8ff4a6e31fdc02c61cbf42039fe7
ham <ham@ham-ThinkPad.(none)>
2011-03-08 22:45:34 +0100

    basic async structure

--- core/bin.js
+++ core/bin.js
@@ -1,16 +1,16 @@
 var bin = new FF("d", "bin", new function() {
-	this.help = new FF("-", "help", function(par, stream) {
+	this.help = new FF("-", "help", function(par, stream, next) {
 		var out = "";
 		for (var f in bin.content) {
 			out += "<div>" + f + "</div>"
 		}
 		stream.out.write(out);
-		return true;
+		next();
 	});
 
-	this.pwd = new FF("-", "pwd", function(par, stream) {
+	this.pwd = new FF("-", "pwd", function(par, stream, next) {
 		stream.out.write(pwd());
-		return true;
+		next();
 	});
 
 	this.ls = new FF("-", "ls", function(par, stream, next) {
@@ -28,57 +28,52 @@ var bin = new FF("d", "bin", new function() {
 			}
 		}
 		stream.out.write(out);
-		//alert(JSON.stringify(next));
 		next();
 	});
 
-	this.mkdir = new FF("-", "mkdir", function(par, stream) {
-		if (par.length < 2) return false;
+	this.mkdir = new FF("-", "mkdir", function(par, stream, next) {
+		if (par.length < 2) return next();
 
 		currentFolder.last().content[par[1]] = new FF("d", par[1]);
 
-		return true;
+		next();
 	});
 
-	this.cd = new FF("-", "cd", function(par, stream) {
-		if (par.length < 2) {
-			return false;
-		}
+	this.cd = new FF("-", "cd", function(par, stream, next) {
+		if (par.length < 2) return next();
 		
 		var tmp = openFolder(par[1], currentFolder);
 		if (tmp)
 			currentFolder = tmp;
-		else
-			return false;
-			
-		return true;
+
+		next();
 	});
 
-	this.touch = new FF("-", "touch", function(par, stream) {	//TODO rewrite
-		if (par.length < 2) return false;
+	this.touch = new FF("-", "touch", function(par, stream, next) {	//TODO rewrite
+		if (par.length < 2) return next();
 
 		currentFolder.last().content[par[1]] = new FF("-", par[1], "");
 
-		return true;
+		next();
 	});
 
-	this.echo = new FF("-", "echo", function(par, stream) {
-		if (par.length < 2) return false;
+	this.echo = new FF("-", "echo", function(par, stream, next) {
+		if (par.length < 2) return next();
 		
 		stream.out.write(par[1]);
 		
-		return true;
+		next();
 	});
 
-	this.cat = new FF("-", "cat", function(par, stream) {
-		if (par.length < 2) return false;
+	this.cat = new FF("-", "cat", function(par, stream, next) {
+		if (par.length < 2) return next();
 
 		var fs = new FileStream(openFile(par[1])).read();
-		if (fs === undefined) return false;
+		if (fs !== undefined) {
+			stream.out.write(fs);
+		}
 		
-		stream.out.write(fs);
-		
-		return true;
+		next();
 	});
 	
 });
